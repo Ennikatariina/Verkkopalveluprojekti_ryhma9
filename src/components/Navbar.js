b324c5ce0ebf810bc2c33346de352f0193ac3b25
EnniKelloniemi <enni.kelloniemi@gmail.com>
2022-11-09 18:55:20 +0200

    tiedostot githubiin

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">LOGO</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Etusivu</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Tiskin takana</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Hinnasto</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Tuotteet
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Juomat</a></li>
+                <li><a className="dropdown-item" href="#">Makeat</a></li>
+                <li><a className="dropdown-item" href="#">Suolaiset</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
