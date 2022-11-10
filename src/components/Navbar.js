50a2fe2f68dcd4a4b591f54bc1b5d58b76664df0
Jennynny <101732141+Jennynny@users.noreply.github.com>
2022-11-10 12:09:39 +0200

    fontin lisäys html ja testaus navbariin (Sumppi).

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">LOGO</a>
+        <a className="navbar-brand" href="#">Sumppi</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
