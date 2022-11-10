9d25fa979c73d45f552c8886bca8c346a445a8b6
Jennynny <101732141+Jennynny@users.noreply.github.com>
2022-11-10 12:50:18 +0200

    Image luominen, testikuvat. Header-koodi (main)

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <main >
+        <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light" id="taustakuva">
+          <div className="col-md-5 p-lg-5 mx-auto my-5">
+            <h1 className="display-4 fw-normal">Sumppi</h1>
+            <p className="lead fw-normal">Maku joka muistetaan</p>
+            <a className="btn btn-outline-secondary" href="#">Verkkokauppaan!</a>
+          </div>
+          <div className="product-device shadow-sm d-none d-md-block"></div>
+          <div className="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+        </div> 
+      </main>
   )
 }
