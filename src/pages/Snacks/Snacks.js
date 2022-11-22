387e1d12cd60fb84be26bec46bd5180db83d3bca
Jennynny <101732141+Jennynny@users.noreply.github.com>
2022-11-22 20:47:43 +0200

    koodin lisääminen snackseihin

--- src/pages/Snacks/Snacks.js
+++ src/pages/Snacks/Snacks.js
@@ -3,6 +3,291 @@ import './stylesnacks.css'
 
 export default function Snacks() {
   return (
-    <div>Snacks</div>
+    
+    <div>
+       
+    
+    <div className='h2-text'>Kahvit</div>
+      <div className="container-lg">
+        <div className="row mx-5 mt-4">
+              
+          <div className='col-sm-4'>
+            <div className="w-100 px-2">
+              <div className="card border-0">
+                <img src={picture2} className="card-img-top" alt="kuva" />
+                  <div className="card-body">
+                  <div className="row mb-2">
+                    <div className="col">
+                      <h5 className="mb-0">Lorem ipsum dolor sit amet consectetur</h5>
+                    </div>
+                    <div className="col-auto">
+                      <span className="fs-4 font-serif text-black">$18</span>
+                    </div>
+                  </div>
+                  <p className="mb-0">
+                    In perspiciatis nisi dicta, aperiam magnam eaque laborum iste.
+                  </p>
+                  <button className="w-50 mb-2 btn btn-sm rounded-3 btn-primary" type="submit">Lisää ostoskoriin</button>
+                </div>
+              </div>
+            </div>
+          </div>
+  
+          <div className='col-sm-4'>
+            <div className="w-100 px-2">
+      
+              <div className="card border-0">
+      
+                <img src={picture2} className="card-img-top" alt="kuva" />
+      
+                <div className="card-body">
+      
+                  <div className="row mb-2">
+                    <div className="col">
+                      <h5 className="mb-0">Lorem ipsum dolor sit amet consectetur</h5>
+                    </div>
+                    <div className="col-auto">
+                      <span className="fs-4 font-serif text-black">$18</span>
+                    </div>
+                  </div>
+      
+                  <p className="mb-0">
+                    In perspiciatis nisi dicta, aperiam magnam eaque laborum iste.
+                  </p>
+                  <button className="w-50 mb-2 btn btn-sm rounded-3 btn-primary" type="submit">Lisää ostoskoriin</button>
+                </div>
+              </div>
+            </div>
+  
+          </div>
+  
+          <div className='col-sm-4'>
+            <div className="w-100 px-2">
+      
+              <div className="card border-0">
+      
+                <img src={picture2} className="card-img-top" alt="kuva" />
+      
+                <div className="card-body">
+      
+                  <div className="row mb-2">
+                    <div className="col">
+                      <h5 className="mb-0">Lorem ipsum dolor sit amet consectetur</h5>
+                    </div>
+                    <div className="col-auto">
+                      <span className="fs-4 font-serif text-black">$18</span>
+                    </div>
+                  </div>
+      
+                  <p className="mb-0">
+                    In perspiciatis nisi dicta, aperiam magnam eaque laborum iste.
+                  </p>
+                  <button className="w-50 mb-2 btn btn-sm rounded-3 btn-primary" type="submit">Lisää ostoskoriin</button>
+                </div>
+              </div>
+            </div>
+  
+          </div>
+  
+          <div className='col-sm-4'>
+            <div className="w-100 px-2">
+      
+              <div className="card border-0">
+      
+                <img src={picture2} className="card-img-top" alt="kuva" />
+      
+                <div className="card-body">
+      
+                  <div className="row mb-2">
+                    <div className="col">
+                      <h5 className="mb-0">Lorem ipsum dolor sit amet consectetur</h5>
+                    </div>
+                    <div className="col-auto">
+                      <span className="fs-4 font-serif text-black">$18</span>
+                    </div>
+                  </div>
+      
+                  <p className="mb-0">
+                    In perspiciatis nisi dicta, aperiam magnam eaque laborum iste.
+                  </p>
+                  <button className="w-50 mb-2 btn btn-sm rounded-3 btn-primary" type="submit">Lisää ostoskoriin</button>
+                </div>
+              </div>
+            </div>
+  
+          </div>
+  
+          <div className='col-sm-4'>
+            <div className="w-100 px-2">
+      
+              <div className="card border-0">
+      
+                <img src={picture2} className="card-img-top" alt="kuva" />
+      
+                <div className="card-body">
+      
+                  <div className="row mb-2">
+                    <div className="col">
+                      <h5 className="mb-0">Lorem ipsum dolor sit amet consectetur</h5>
+                    </div>
+                    <div className="col-auto">
+                      <span className="fs-4 font-serif text-black">$18</span>
+                    </div>
+                  </div>
+      
+                  <p className="mb-0">
+                    In perspiciatis nisi dicta, aperiam magnam eaque laborum iste.
+                  </p>
+                  <button className="w-50 mb-2 btn btn-sm rounded-3 btn-primary" type="submit">Lisää ostoskoriin</button>
+                </div>
+              </div>
+            </div>
+  
+          </div>
+            
+        </div>
+      </div>
+  
+    <div className='h2-text'>Teet ja muut kuumat juomat</div>
+      <div className="container-lg">
+        <div className="row mx-5 mt-4">
+              
+          <div className='col-sm-4'>
+            <div className="w-100 px-2">
+              <div className="card border-0">
+                <img src={picture2} className="card-img-top" alt="kuva" />
+                  <div className="card-body">
+                  <div className="row mb-2">
+                    <div className="col">
+                      <h5 className="mb-0">Lorem ipsum dolor sit amet consectetur</h5>
+                    </div>
+                    <div className="col-auto">
+                      <span className="fs-4 font-serif text-black">$18</span>
+                    </div>
+                  </div>
+                  <p className="mb-0">
+                    In perspiciatis nisi dicta, aperiam magnam eaque laborum iste.
+                  </p>
+                  <button className="w-50 mb-2 btn btn-sm rounded-3 btn-primary" type="submit">Lisää ostoskoriin</button>
+                </div>
+              </div>
+            </div>
+          </div>
+  
+          <div className='col-sm-4'>
+            <div className="w-100 px-2">
+      
+              <div className="card border-0">
+      
+                <img src={picture2} className="card-img-top" alt="kuva" />
+      
+                <div className="card-body">
+      
+                  <div className="row mb-2">
+                    <div className="col">
+                      <h5 className="mb-0">Lorem ipsum dolor sit amet consectetur</h5>
+                    </div>
+                    <div className="col-auto">
+                      <span className="fs-4 font-serif text-black">$18</span>
+                    </div>
+                  </div>
+      
+                  <p className="mb-0">
+                    In perspiciatis nisi dicta, aperiam magnam eaque laborum iste.
+                  </p>
+                  <button className="w-50 mb-2 btn btn-sm rounded-3 btn-primary" type="submit">Lisää ostoskoriin</button>
+                </div>
+              </div>
+            </div>
+  
+          </div>
+  
+          <div className='col-sm-4'>
+            <div className="w-100 px-2">
+      
+              <div className="card border-0">
+      
+                <img src={picture2} className="card-img-top" alt="kuva" />
+      
+                <div className="card-body">
+      
+                  <div className="row mb-2">
+                    <div className="col">
+                      <h5 className="mb-0">Lorem ipsum dolor sit amet consectetur</h5>
+                    </div>
+                    <div className="col-auto">
+                      <span className="fs-4 font-serif text-black">$18</span>
+                    </div>
+                  </div>
+      
+                  <p className="mb-0">
+                    In perspiciatis nisi dicta, aperiam magnam eaque laborum iste.
+                  </p>
+                  <button className="w-50 mb-2 btn btn-sm rounded-3 btn-primary" type="submit">Lisää ostoskoriin</button>
+                </div>
+              </div>
+            </div>
+  
+          </div>
+  
+          <div className='col-sm-4'>
+            <div className="w-100 px-2">
+      
+              <div className="card border-0">
+      
+                <img src={picture2} className="card-img-top" alt="kuva" />
+      
+                <div className="card-body">
+      
+                  <div className="row mb-2">
+                    <div className="col">
+                      <h5 className="mb-0">Lorem ipsum dolor sit amet consectetur</h5>
+                    </div>
+                    <div className="col-auto">
+                      <span className="fs-4 font-serif text-black">$18</span>
+                    </div>
+                  </div>
+      
+                  <p className="mb-0">
+                    In perspiciatis nisi dicta, aperiam magnam eaque laborum iste.
+                  </p>
+                  <button className="w-50 mb-2 btn btn-sm rounded-3 btn-primary" type="submit">Lisää ostoskoriin</button>
+                </div>
+              </div>
+            </div>
+  
+          </div>
+  
+          <div className='col-sm-4'>
+            <div className="w-100 px-2">
+      
+              <div className="card border-0">
+      
+                <img src={picture2} className="card-img-top" alt="kuva" />
+      
+                <div className="card-body">
+      
+                  <div className="row mb-2">
+                    <div className="col">
+                      <h5 className="mb-0">Lorem ipsum dolor sit amet consectetur</h5>
+                    </div>
+                    <div className="col-auto">
+                      <span className="fs-4 font-serif text-black">$18</span>
+                    </div>
+                  </div>
+      
+                  <p className="mb-0">
+                    In perspiciatis nisi dicta, aperiam magnam eaque laborum iste.
+                  </p>
+                  <button className="w-50 mb-2 btn btn-sm rounded-3 btn-primary" type="submit">Lisää ostoskoriin</button>
+                </div>
+              </div>
+            </div>
+  
+          </div>
+            
+        </div>
+      </div>
+    </div>
   )
 }
