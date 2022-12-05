5630fc457cd34a503f0ea025996ea7022ae70b94
Jennynny <101732141+Jennynny@users.noreply.github.com>
2022-12-05 15:55:43 +0200

    product & productCard

---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import picture2 from '../../images/kahvia.jpg'
+import './ProductCard.css'
+
+export default function ProductCard(props){
+
+
+    return(
+        <div className='col-sm-4'>
+        <div className="w-100 px-2">
+          <div className="card border-0">
+            <img src={picture2} className="card-img-top" alt="kuva" />
+              <div className="card-body">
+              <div className="row mb-2">
+                <div className="col">
+                  <h5 className="mb-0">{props.param.tuotenimi}</h5>
+                </div>
+                <div className="col-auto">
+                  <span className="fs-4 font-serif text-black">{props.param.hinta}€</span>
+                </div>
+              </div>
+              <p className="mb-0">
+                {props.param.kuvaus}
+              </p>
+              <button className="w-50 mb-2 btn btn-sm rounded-3 btn-primary" type="submit">Lisää ostoskoriin</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+} 
+
+
