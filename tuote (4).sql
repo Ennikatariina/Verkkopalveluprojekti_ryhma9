29d937204053805eb36e96858b2b6fa8c5cbbc34
Jennynny <101732141+Jennynny@users.noreply.github.com>
2022-12-07 19:27:26 +0200

    tuotetaulun viiteavaimen lisäys

--- tuote (4).sql
+++ tuote (4).sql
@@ -82,6 +82,9 @@ ALTER TABLE `tuote`
 --
 ALTER TABLE `tuote`
   MODIFY `tuotenro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+  ALTER TABLE `tuote`
+   ADD CONSTRAINT `tuote_viiteavain` FOREIGN KEY (`tuoteryhmanro`) REFERENCES `tuoteryhma` (`tuoteryhmanro`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
