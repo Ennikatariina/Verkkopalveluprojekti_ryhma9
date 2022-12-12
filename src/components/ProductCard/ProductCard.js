a5b42d31972c4c7eedaccb0bd34973a13d3a7742
Jennynny <101732141+Jennynny@users.noreply.github.com>
2022-12-12 11:26:47 +0200

    Tuotekuvat tuotekortteihin ja muotoilua css

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,15 +1,20 @@
 import React from "react";
-import picture2 from '../../images/kahvia.jpg'
+//import picture2 from '../../images/kahvia.jpg'
 import './ProductCard.css'
+
 
 export default function ProductCard(props){
 
+  const URL ="http://localhost/verkkokauppa_backend"
 
     return(
         <div className='col-sm-4'>
         <div className="w-100 px-2">
           <div className="card border-0">
-            <img src={picture2} className="card-img-top" alt="kuva" />
+           {/* <img src={picture2} className="card-img-top" alt="kuva" />*/}
+          <img src={URL + '/imagess/' + props.param.kuvannimi} className="card-img-top productImg" alt="kuva" />
+           
+           {/*<div className="card-img-top"  alt="tuotekuva">{props.param.kuvannimi} </div>*/}
               <div className="card-body">
               <div className="row mb-2">
                 <div className="col">
@@ -19,7 +24,7 @@ export default function ProductCard(props){
                   <span className="fs-4 font-serif text-black">{props.param.hinta}€</span>
                 </div>
               </div>
-              <p className="mb-0">
+              <p className="mb-0 productText">
                 {props.param.kuvaus}
               </p>
               <button className="w-50 mb-2 btn btn-sm rounded-3 btn-primary" type="submit" onClick={e => props.addToShoppingbasket(props.param)}>Lisää ostoskoriin</button>
