91cf408371f9018a681702e5be6882ff5dc604d4
Jennynny <101732141+Jennynny@users.noreply.github.com>
2022-12-12 22:46:16 +0200

    Avataret MadeBy sivulle

--- src/pages/MadeBy/MadeBy.js
+++ src/pages/MadeBy/MadeBy.js
@@ -1,10 +1,60 @@
 import React from 'react'
+import picture4 from '../../images/Avatarit/Aikku_avatar.png'
+import picture5 from '../../images/Avatarit/Avatar_Enni.png'
+import picture6 from '../../images/Avatarit/Avatar_JennyK.jpg'
+import picture7 from '../../images/Avatarit/Jonnaavatar.png'
 
 export default function MadeBy() {
     return (
     <div>
 <h1>Made By</h1>
 <p>Tähän tekijät ja avataret?</p>
+
+<div className="container-fluid">
+  <div className="row mx-5 mt-4">
+    
+<div className="col-md-12  col-lg-3">
+      <div className="card" style={{width: "auto"}}>
+        <img src={picture4} className="img-thumbnail" alt="..."/>
+        <div className="card-body">
+          <h3>Aila-Mari</h3>
+          <p className="card-text">juu</p>
+        </div>
+      </div>
+    </div>
+    <div className="col-md-12 col-lg-3">
+      <div className="card" style={{width: "auto"}}>
+        <img src={picture5} className="img-thumbnail" alt="..."/>
+        <div className="card-body">
+          <h3>Enni</h3>
+          <p className="card-text"> juu
+          </p>
+        </div> 
+      </div>
+    </div>
+    <div className="col-md-12 col-lg-3">
+      <div className="card" style={{width: "auto"}}>
+        <img src={picture6} className="img-thumbnail" alt="..."/>
+        <div className="card-body">
+          <h3>Jenny</h3>
+          <p className="card-text">juu 
+             </p>
+        </div>
+      </div>
+    </div>
+  
+    <div className="col-md-12 col-lg-3">
+      <div className="card" style={{width:"auto"}}>
+        <img src={picture7} className="img-thumbnail" alt="..."/>
+        <div className="card-body">
+          <h3>Jonna</h3>
+          <p className="card-text">juu</p>
+        </div>
+      </div>
+    </div>
 </div>
+</div>
+</div>
+
 )
 }
